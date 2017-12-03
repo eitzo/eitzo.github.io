d410d48b9acc9ac70955d6741cd09b7da83d547e
eitzo <eitzo@hotmail.com>
2017-12-03 12:34:35 +0100

    stgk vo update

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,7 @@ setTimeout(function(){
 setInterval(function(){
     var progress = document.getElementById('custom-progress');
 
-    if (progress.value < 57) {
+    if (progress.value < 58) {
         progress.value += 1;
     }
 
