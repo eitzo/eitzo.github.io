6b4c36bcf26d254634cd0c4d6f8272ad53284d00
eitzo <eitzo@hotmail.com>
2018-01-12 13:17:05 +0100

    estue

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,7 @@ setTimeout(function(){
 setInterval(function(){
     var progress = document.getElementById('custom-progress');
 
-    if (progress.value < 59) {
+    if (progress.value < 60) {
         progress.value += 1;
     }
 
