561142af5db3f6733fb099588bd42bf60545a85c
eitzo <eitzo@hotmail.com>
2018-02-09 11:53:58 +0100

    msp

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,7 @@ setTimeout(function(){
 setInterval(function(){
     var progress = document.getElementById('custom-progress');
 
-    if (progress.value < 60) {
+    if (progress.value < 61) {
         progress.value += 1;
     }
 
