104ced58ab203ee512ab3dc054c97fa57a7677f5
eitzo <eitzo@hotmail.com>
2019-07-08 22:12:21 +0200

    fix progressbar

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,7 @@ setTimeout(function(){
 setInterval(function(){
     var progress = document.getElementById('custom-progress');
 
-    if (progress.value < 62) {
+    if (progress.value < 63) {
         progress.value += 1;
     }
 
