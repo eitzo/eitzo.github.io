74576be09089310e2bd8aed4d9f3345a3bf4405c
eitzo <eitzo@hotmail.com>
2020-02-10 16:03:45 +0100

    ST

--- js/anim.js
+++ js/anim.js
@@ -2,7 +2,7 @@ setTimeout(function(){
 setInterval(function(){
     var progress = document.getElementById('custom-progress');
 
-    if (progress.value < 64) {
+    if (progress.value < 65) {
         progress.value += 1;
     }
 
